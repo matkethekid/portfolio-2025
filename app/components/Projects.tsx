cfa1180a03e1f5e222b107bf65a3985a56fba1ae
mateja <markojovanovicc23@outlook.com>
2025-11-26 21:46:40 +0100

    initialized new design for seeing details on every project

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -12,7 +12,6 @@ import precizna from "../../public/precizna.webp";
 import aistartup from "../../public/aistartup.webp";
 import hikariagency from '../../public/hikariagency.webp';
 import { motion } from "motion/react";
-import { useInView } from "react-intersection-observer";
 
 interface Project {
     id: number;
@@ -68,24 +67,40 @@ const projects: Project[] = [
 ];
 
 const ProjectsPage = () => {
-    const { ref, inView } = useInView({
-        triggerOnce: true,
-        threshold: 0.2,
-    });
     const router = useRouter();
     const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
     const [isActive, setIsActive] = useState<boolean>(false);
+    const [isProjectOpen, setIsProjectOpen] = useState<boolean>(false);
+    const [currentProject, setCurrentProject] = useState<any>([]);
 
     function toggleMenu() {
         setIsActive(prevState => !prevState);
         setIsSidebarOpen(prevState => !prevState);
+    }
+
+    function seeProjectDetails(index: number) {
+        setIsProjectOpen(true);
+        setCurrentProject(projects[index]);
+        document.body.style.overflowY = 'hidden';
+    }
+
+    function closeProject() {
+        setIsProjectOpen(false);
+        setCurrentProject([]);
+        document.body.style.overflowY = 'auto';
     }
   return (
     <div className='flex flex-1 flex-col w-full min-h-screen max-sm:px-0 px-7 bg-transparent py-7 max-w-5xl max-sm:max-w-8xl mx-auto z-20 relative'>
       <div className="px-7">
         <Navbar isSidebarOpen={isSidebarOpen}/>
       </div>
-      <div>
+        <div className={isProjectOpen ? 'bg-black w-full h-full fixed inset-0 opacity-[0.8] z-40' : 'hidden'}></div>
+        <div className={isProjectOpen ? 'fixed inset-0 flex items-center justify-center z-50' : 'hidden'} onClick={closeProject}>
+            <div className="bg-red-500 w-[50%] h-[50%] flex items-center justify-center rounded-lg">
+                <p className="text-white">Project Details</p>
+            </div>
+        </div>
+        <div>
         <motion.button initial={{ y: 0, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: .3 }} onClick={() => toggleMenu()} className="md:hidden bg-neutral-500/30 absolute top-14 right-7 w-10 h-10 flex items-center justify-center rounded-xl transition-all duration-200 border border-neutral-500 group z-23" aria-label="Toggle menu">
             <div className="relative flex flex-col items-center justify-center w-5 h-5 overflow-hidden">
               <span className={`absolute w-5 h-[2px] bg-white/50 rounded-full transform transition-transform duration-300 ease-in-out ${isActive ? "rotate-45" : "-translate-y-1.5"}`}></span>
@@ -102,7 +117,7 @@ const ProjectsPage = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 gap-6 w-full mt-30 px-4">
             {
                 projects.map((project, index) => (
-                    <div key={index} className="w-full rounded-lg group cursor-pointer bg-gradient-to-br from-emerald-500/60 to-black border border-emerald-500/20 hover:border-emerald-500/80 transition-all duration-300 flex flex-col" onClick={() => router.push("https://hikariagency.org/")}>
+                    <div key={index} onClick={() => seeProjectDetails(index)} className="w-full rounded-lg group cursor-pointer bg-gradient-to-br from-emerald-500/60 to-black border border-emerald-500/20 hover:border-emerald-500/80 transition-all duration-300 flex flex-col">
                         <div className="w-full h-full aspect-video relative mt-0">
                             <Image
                                 src={project.image}
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="relative flex min-h-screen w-full bg-emerald-950/60 dark:bg-emerald-950/60">
       <div className="absolute inset-0 
         [background-size:12px_12px]
-        [background-image:linear-gradient(to_right,rgba(16,185,129,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(16,185,129,0.08)_1px,transparent_1px)] ">
+        [background-image:linear-gradient(to_right,rgba(16,185,129,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(16,185,129,0.08)_1px,transparent_1px)]">
       </div>
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_70%,black)] bg-black"></div>
       <HomePage/>
