7f927d9bac722f5a5e3d012cbe9a306914d0d8c8
mateja <markojovanovicc23@outlook.com>
2025-11-27 09:26:28 +0100

    new design for seeing project in progress

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -96,8 +96,25 @@ const ProjectsPage = () => {
       </div>
         <div className={isProjectOpen ? 'bg-black w-full h-full fixed inset-0 opacity-[0.8] z-40' : 'hidden'}></div>
         <div className={isProjectOpen ? 'fixed inset-0 flex items-center justify-center z-50' : 'hidden'} onClick={closeProject}>
-            <div className="bg-red-500 w-[50%] h-[50%] flex items-center justify-center rounded-lg">
-                <p className="text-white">Project Details</p>
+            <div className="w-full max-w-5xl h-[80%] flex flex-col-reverse lg:flex-row items-center justify-center rounded-lg">
+                <div className="rounded-l-[10px] w-full h-full relative p-1">
+                    <Image src={currentProject.image} alt={'image'} className="w-full h-full object-cover flex z-1 rounded-[10px]"/>
+                    <div className={'bg-linear-to-r from-black/1 via-black/50 to-black w-[40%] h-full opacity-[0.8] z-20 top-0 right-0 absolute'}></div>
+                </div>
+                <div className="bg-[#06070a] rounded-[10px] opacity-[1] rounded-r-[10px] w-full h-full flex flex-col gap-10 pt-7 pb-7 pl-7 pr-7">
+                    <h1 className="text-4xl font-bold">{currentProject.title}</h1>
+                    <div className="w-[90%] h-[2px] mx-auto bg-linear-to-r from-black via-emerald-950 to-black"></div>
+                    <p className="text-slate-300 text-md">{currentProject.description}</p>
+                    <div className="flex flex-col w-full">
+                        <h2 className="text-emerald-600">SERVICES</h2>
+                        <div className="flex flex-wrap gap-5">
+                            <p>aaaaa</p>
+                            <p>asdasdasdasd</p>
+                            <p>asdasdasdasdasd</p>
+                            <p>asddddasdasdasdada</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div>
@@ -117,7 +134,7 @@ const ProjectsPage = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 gap-6 w-full mt-30 px-4">
             {
                 projects.map((project, index) => (
-                    <div key={index} onClick={() => seeProjectDetails(index)} className="w-full rounded-lg group cursor-pointer bg-gradient-to-br from-emerald-500/60 to-black border border-emerald-500/20 hover:border-emerald-500/80 transition-all duration-300 flex flex-col">
+                    <div key={index} onClick={() => seeProjectDetails(index)} className="w-full rounded-lg group cursor-pointer bg-linear-to-br from-emerald-500/60 to-black border border-emerald-500/20 hover:border-emerald-500/80 transition-all duration-300 flex flex-col">
                         <div className="w-full h-full aspect-video relative mt-0">
                             <Image
                                 src={project.image}
